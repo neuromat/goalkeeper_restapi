371021d94ea0296dad9bdb8f94a1b9dbdbfd7a48
carlosribas <caduribas@gmail.com>
2019-05-31 17:36:21 -0300

    Create a Profile model to connect User and Level

--- goalkeeper/custom_user/models.py
+++ goalkeeper/custom_user/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from game.models import Level
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
