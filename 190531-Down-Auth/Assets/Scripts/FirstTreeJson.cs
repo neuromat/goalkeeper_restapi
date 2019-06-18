1cbed3755a8a7ee8e7d8046592ad0560cd5e3724
Éder Porto <eder.porto@hotmail.com>
2019-06-18 10:52:35 -0300

    Implementing functionality to import contexts and trees from database, allow to play them and save the results in the database - adding missing script

--- 190531-Down-Auth/Assets/Scripts/FirstTreeJson.cs
+++ 190531-Down-Auth/Assets/Scripts/FirstTreeJson.cs
@@ -4,33 +4,35 @@ using System.Collections.Generic;
 
 public class FirstTreeJson
 {
-    public bool sendMarkersToEEG;
-    public string portalEEGserial;
-    public bool showPlayPauseButton;
-    public string? leftInputKey;
-    public string? centerInputKey;
-    public string? rightInputKey;
-    public string institution;
-    public bool attentionPoint;
-    public float attentionDiameter;
-    public string attentionColorStart;
-    public string attentionColorCorrect;
-    public string attentionColorWrong;
-    public List<MenuJson> menus;
-    public string id;
-    public int limitPlays;
-    public int choices;
-    public int depth;
-    public bool readSequ;
-    public string sequ;
-    public string sequR;
-    public int minHitsInSequence;
-    public string animationTypeJG;
-    public string animationTypeOthers;
-    public bool scoreboard;
-    public string finalScoreboard;
-    public int playsToRelax;
-    public bool showHistory;
-    public float sppedGKAnim;
-    public List<JsonStateInput> states;
+    public string id { get; set; }
+    public int? limitPlays { get; set; }
+    public int choices { get; set; }
+    public int? depth { get; set; }
+    public bool readSequ { get; set; }
+    public string sequ { get; set; }
+    public string sequR { get; set; }
+    public List<JsonStateInput> states { get; set; }
+    public int? minHitsInSequence { get; set; }
+    public string animationTypeJG { get; set; }
+    public string animationTypeOthers { get; set; }
+    public bool scoreboard { get; set; }
+    public string groupCode { get; set; }
+    public string finalScoreboard { get; set; }
+    public int playsToRelax { get; set; }
+    public bool showHistory { get; set; }
+    public string sendMarkersToEEG { get; set; }
+    public string portalEEGserial { get; set; }
+    public bool showPlayPauseButton { get; set; }
+    public string pausePlaynputKey { get; set; }
+    public string leftInputKey { get; set; }
+    public string centerInputKey { get; set; }
+    public string rightInputKey { get; set; }
+    public string institution { get; set; }
+    public bool attentionPoint { get; set; }
+    public double attentionDiameter { get; set; }
+    public string attentionColorStart { get; set; }
+    public string attentionColorCorrect { get; set; }
+    public string attentionColorWrong { get; set; }
+    public double speedGKAnim { get; set; }
+    public List<MenuJson> menus { get; set; }
 }
