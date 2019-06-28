4cc73c7d0512bfaca9fa3e8a60d15a5985b8c2a2
carlosribas <caduribas@gmail.com>
2019-06-28 11:21:16 -0300

    New tests

--- goalkeeper/game/tests/test_views.py
+++ goalkeeper/game/tests/test_views.py
@@ -5,8 +5,8 @@ from django.test import TestCase
 
 from game.views import goalkeeper_game_new, goalkeeper_game_view, goalkeeper_game_update, goalkeeper_game_list, \
     context_tree, available_context, game_config_new, game_config_list, game_config_view, game_config_update, \
-    check_contexts_without_probability, home
-from game.models import Context, GameConfig, GoalkeeperGame, Level
+    check_contexts_without_probability, probability, probability_update
+from game.models import Context, GameConfig, GoalkeeperGame, Level, Probability
 
 USER_USERNAME = 'user'
 USER_PWD = 'mypassword'
@@ -32,11 +32,17 @@ class GameTest(TestCase):
                                       plays_to_relax=0, player_time=1.0, celebration_time=1.0, read_seq=True,
                                       final_score_board='short', play_pause=True, score_board=True, show_history=True)
 
+    ######################
+    # Home page
+    ######################
     def test_home_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
+    ######################
+    # List game config
+    ######################
     def test_game_config_list_status_code(self):
         url = reverse('game_config_list')
         response = self.client.get(url)
@@ -47,6 +53,9 @@ class GameTest(TestCase):
         view = resolve('/game/config/list/')
         self.assertEquals(view.func, game_config_list)
 
+    ######################
+    # New game config
+    ######################
     def test_game_config_new_status_code(self):
         url = reverse('game_config_new')
         response = self.client.get(url)
@@ -73,6 +82,9 @@ class GameTest(TestCase):
         self.assertEqual(game.count(), 1)
         self.assertTrue(isinstance(game[0], GameConfig))
 
+    ######################
+    # View game config
+    ######################
     def test_game_config_view_status_code(self):
         config = GameConfig.objects.first()
         response = self.client.get(reverse("game_config_view", args=(config.id,)))
@@ -105,6 +117,9 @@ class GameTest(TestCase):
         self.assertEqual(str(message[0]), "Kicker removed successfully.")
         self.assertEqual(len(GoalkeeperGame.objects.all()), 0)
 
+    ######################
+    # Update game config
+    ######################
     def test_game_config_update_status_code(self):
         config = GameConfig.objects.first()
         response = self.client.get(reverse("game_config_update", args=(config.id,)))
@@ -147,6 +162,9 @@ class GameTest(TestCase):
         self.assertEqual(len(message), 1)
         self.assertEqual(str(message[0]), "There are no changes to save.")
 
+    ######################
+    # List goalkeeper game
+    ######################
     def test_goalkeeper_game_list_status_code(self):
         url = reverse('goalkeeper_game_list')
         response = self.client.get(url)
@@ -157,6 +175,9 @@ class GameTest(TestCase):
         view = resolve('/game/goalkeeper/list/')
         self.assertEquals(view.func, goalkeeper_game_list)
 
+    ######################
+    # New goalkeeper game
+    ######################
     def test_goalkeeper_game_new_status_code(self):
         url = reverse('goalkeeper_game_new')
         response = self.client.get(url)
@@ -172,7 +193,6 @@ class GameTest(TestCase):
         url = reverse('goalkeeper_game_new')
         self.data = {
             'config': config.pk,
-            'phase': 1,
             'sequence': '',
             'depth': 3,
             'number_of_directions': 3,
@@ -193,6 +213,9 @@ class GameTest(TestCase):
         self.assertEqual(game.count(), 1)
         self.assertTrue(isinstance(game[0], GoalkeeperGame))
 
+    ######################
+    # View goalkeeper game
+    ######################
     def test_goalkeeper_game_view_status_code(self):
         game = GoalkeeperGame.objects.first()
         response = self.client.get(reverse("goalkeeper_game_view", args=(game.id,)))
@@ -212,6 +235,9 @@ class GameTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(GoalkeeperGame.objects.count(), 0)
 
+    ########################
+    # Update goalkeeper game
+    ########################
     def test_goalkeeper_game_update_status_code(self):
         game = GoalkeeperGame.objects.first()
         response = self.client.get(reverse("goalkeeper_game_update", args=(game.id,)))
@@ -227,7 +253,6 @@ class GameTest(TestCase):
         game = GoalkeeperGame.objects.first()
         self.data = {
             'config': config.pk,
-            'phase': 1,
             'sequence': '',
             'depth': 4,
             'number_of_directions': 3,
@@ -248,6 +273,33 @@ class GameTest(TestCase):
         game_update = GoalkeeperGame.objects.filter(depth=4, read_seq=False)
         self.assertEqual(game_update.count(), 1)
 
+    def test_goalkeeper_game_update_no_changes(self):
+        config = GameConfig.objects.first()
+        game = GoalkeeperGame.objects.first()
+        self.data = {
+            'config': config.pk,
+            'sequence': '',
+            'depth': 2,
+            'number_of_directions': 3,
+            'number_of_plays': 10,
+            'plays_to_relax': 0,
+            'player_time': 1.0,
+            'celebration_time': 1.0,
+            'final_score_board': 'short',
+            'play_pause': True,
+            'score_board': True,
+            'show_history': True,
+            'create_seq_manually': 'no',
+            'action': 'save'
+        }
+        response = self.client.post(reverse("goalkeeper_game_update", args=(game.id,)), self.data)
+        message = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(message), 1)
+        self.assertEqual(str(message[0]), "There are no changes to save.")
+
+    ########################
+    # Function context tree
+    ########################
     def test_context_tree_status_code(self):
         game = GoalkeeperGame.objects.first()
         response = self.client.get(reverse("context", args=(game.id,)))
@@ -258,6 +310,47 @@ class GameTest(TestCase):
         view = resolve('/game/goalkeeper/context/1/')
         self.assertEquals(view.func, context_tree)
 
+    def test_context_tree(self):
+        game = GoalkeeperGame.objects.first()
+        self.data = {
+            'goalkeeper': game.id,
+            '0': 'True',
+            '1': 'True',
+            '2': 'True',
+            'action': 'save'
+        }
+        self.client.post(reverse("context", args=(game.id,)), self.data)
+        self.assertEqual(Context.objects.count(), 3)
+        context_list, context_not_analyzed = available_context(game.id)
+        self.assertListEqual(context_list, [])
+        self.assertFalse(context_not_analyzed.exists())
+
+    def test_context_tree_depth_2(self):
+        game = GoalkeeperGame.objects.first()
+        self.data = {
+            'goalkeeper': game.id,
+            '0': 'True',
+            '1': 'False',
+            '2': 'True',
+            'action': 'save'
+        }
+        self.client.post(reverse("context", args=(game.id,)), self.data)
+        context_list, context_not_analyzed = available_context(game.id)
+        self.assertListEqual(context_list, ['01', '11', '21'])
+        self.assertTrue(context_not_analyzed.exists())
+        self.data2 = {
+            'goalkeeper': game.id,
+            '01': 'True',
+            '11': 'True',
+            '21': 'True',
+            'action': 'save'
+        }
+        self.client.post(reverse("context", args=(game.id,)), self.data2)
+        self.assertEqual(Context.objects.count(), 6)
+
+    ############################
+    # Function available context
+    ############################
     def test_available_context(self):
         game = GoalkeeperGame.objects.first()
         available_contexts, context_not_analyzed = available_context(game.id)
@@ -273,15 +366,9 @@ class GameTest(TestCase):
         self.assertListEqual(available_contexts, ['01', '11', '21'])
         self.assertEqual(context_not_analyzed.count(), 1)
 
-    def test_context_tree(self):
-        game = GoalkeeperGame.objects.first()
-        self.data = {'goalkeeper': game.id, '0': 'True', '1': 'True', '2': 'True', 'action': 'save'}
-        self.client.post(reverse("context", args=(game.id,)), self.data)
-        self.assertEqual(Context.objects.count(), 3)
-        context_list, context_not_analyzed = available_context(game.id)
-        self.assertListEqual(context_list, [])
-        self.assertFalse(context_not_analyzed.exists())
-
+    ############################################
+    # Function check context without probability
+    ############################################
     def test_check_contexts_without_probability(self):
         game = GoalkeeperGame.objects.first()
         Context.objects.create(goalkeeper=game, path='0', is_context='True', analyzed=False)
@@ -293,3 +380,79 @@ class GameTest(TestCase):
         game = GoalkeeperGame.objects.first()
         response = check_contexts_without_probability(game.id)
         self.assertIsNone(response)
+
+    ############################
+    # Probability
+    ############################
+    def test_probability_status_code(self):
+        game = GoalkeeperGame.objects.first()
+        response = self.client.get(reverse("probability", args=(game.id,)))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'game/probability.html')
+
+    def test_probability_url_resolves_probability_view(self):
+        view = resolve('/game/goalkeeper/context/1/probability/')
+        self.assertEquals(view.func, probability)
+
+    def test_probability(self):
+        game = GoalkeeperGame.objects.first()
+        context = Context.objects.create(goalkeeper=game, path='0', is_context='True', analyzed=False)
+        self.data = {
+            'context': context.path,
+            'context-0-0': '',
+            'context-0-1': 1,
+            'context-0-2': '',
+            'action': 'save'
+        }
+        self.client.post(reverse("probability", args=(game.id,)), self.data)
+        self.assertEqual(Probability.objects.count(), 3)
+
+    def test_probability_wrong_values(self):
+        game = GoalkeeperGame.objects.first()
+        context = Context.objects.create(goalkeeper=game, path='0', is_context='True', analyzed=False)
+        self.data = {
+            'context': context.path,
+            'context-0-0': .5,
+            'context-0-1': .4,
+            'context-0-2': .3,
+            'action': 'save'
+        }
+        response = self.client.post(reverse("probability", args=(game.id,)), self.data)
+        message = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(message), 1)
+        self.assertEqual(str(message[0]), "The sum of the probabilities must be equal to 1.")
+
+    ############################
+    # Update probability
+    ############################
+    def test_probability_update_status_code(self):
+        game = GoalkeeperGame.objects.first()
+        context = Context.objects.create(goalkeeper=game, path='0', is_context='True', analyzed=False)
+        response = self.client.get(reverse("probability_update", args=(context.id,)))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'game/probability.html')
+
+    def test_probability_update_url_resolves_probability_updatey_view(self):
+        view = resolve('/game/goalkeeper/context/1/probability/update/')
+        self.assertEquals(view.func, probability_update)
+
+    def test_probability_update(self):
+        game = GoalkeeperGame.objects.first()
+        context = Context.objects.create(goalkeeper=game, path='0', is_context='True', analyzed=False)
+        self.data = {
+            'context': context.path,
+            'context-0-0': .5,
+            'context-0-1': 0,
+            'context-0-2': .5,
+            'action': 'save'
+        }
+        self.client.post(reverse("probability", args=(game.id,)), self.data)
+        self.data_update = {
+            'context': context.path,
+            'context-0-0': 0,
+            'context-0-1': 1,
+            'context-0-2': 0,
+            'action': 'save'
+        }
+        self.client.post(reverse("probability_update", args=(context.id,)), self.data_update)
+        self.assertEqual(Probability.objects.filter(context=context, direction=1, value=1.0).count(), 1)
