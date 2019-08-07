13c6b8b4aeaaf08e401490b6946df8b73b14e73d
Éder Porto <eder.porto@hotmail.com>
2019-08-07 13:29:03 -0300

    Fixing tests - Custom User

--- goalkeeper/custom_user/tests_forms.py
+++ goalkeeper/custom_user/tests_forms.py
@@ -1,53 +1,51 @@
 from django.test import TestCase
 from django.forms import ValidationError
+from faker import Factory
 from .forms import UserForm
 
 from .views import *
 
-USER_USERNAME = 'user'
 USER_PWD = 'mypassword'
-USER_EMAIL = 'user@example.com'
 
 
 class UserFormTests(TestCase):
-
     def test_user_form_is_valid(self):
-        form_data = {'username': USER_USERNAME,
-                     'email': USER_EMAIL,
+        form_data = {'username': self.create_faker_username(),
+                     'email': self.create_faker_email(),
                      'password1': USER_PWD,
                      'password2': USER_PWD}
         form = UserForm(data=form_data)
         self.assertTrue(form.is_valid())
 
     def test_user_form_is_invalid_without_username(self):
-        form_data = {'email': USER_EMAIL,
+        form_data = {'email': self.create_faker_email(),
                      'password1': USER_PWD,
                      'password2': USER_PWD}
         form = UserForm(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_user_form_is_invalid_without_confirm_password(self):
-        form_data = {'username': USER_USERNAME,
+        form_data = {'username': self.create_faker_username(),
                      'password1': USER_PWD}
         form = UserForm(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_user_form_is_invalid_without_password(self):
-        form_data = {'username': USER_USERNAME}
+        form_data = {'username': self.create_faker_username()}
         form = UserForm(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_user_form_is_invalid_with_different_passwords(self):
-        form_data = {'username': USER_USERNAME,
-                     'email': USER_EMAIL,
+        form_data = {'username': self.create_faker_username(),
+                     'email': self.create_faker_email(),
                      'password1': USER_PWD,
                      'password2': USER_PWD + "Test"}
         form = UserForm(data=form_data)
         self.assertFalse(form.is_valid())
 
     def test_clean_password(self):
-        form_data = {'username': USER_USERNAME,
-                     'email': USER_EMAIL,
+        form_data = {'username': self.create_faker_username(),
+                     'email': self.create_faker_email(),
                      'password1': USER_PWD,
                      'password2': USER_PWD}
 
@@ -56,8 +54,8 @@ class UserFormTests(TestCase):
         self.assertNotEqual(form.clean_password(), USER_PWD)
 
     def test_clean_email_already_exists(self):
-        form_data = {'username': USER_USERNAME,
-                     'email': USER_EMAIL,
+        form_data = {'username': self.create_faker_username(),
+                     'email': self.create_faker_email(),
                      'password1': USER_PWD,
                      'password2': USER_PWD}
 
@@ -65,10 +63,20 @@ class UserFormTests(TestCase):
         self.assertTrue(form.is_valid())
         form.save()
 
-        form_data_2 = {'username': USER_USERNAME + "2",
-                       'email': USER_EMAIL,
+        form_data_2 = {'username': self.create_faker_username() + "2",
+                       'email': form_data.get("email"),
                        'password1': USER_PWD,
                        'password2': USER_PWD}
         form = UserForm(data=form_data_2)
         self.assertFalse(form.is_valid())
         self.assertRaises(ValidationError)
+
+    @staticmethod
+    def create_faker_email():
+        faker = Factory.create()
+        return faker.email()
+
+    @staticmethod
+    def create_faker_username():
+        faker = Factory.create()
+        return faker.word()
